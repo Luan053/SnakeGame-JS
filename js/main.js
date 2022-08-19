29f2a978cc1c95390c01b0e6824a6579528093ec
Luan <luanrschulzz@gmail.com>
2022-08-19 12:04:24 -0300

    aleatoriedade da fruta

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ function drawSnake() {
 }
 
 
-function drawfood(){
+function drawfood() {
     context.fillStyle = "red";
     context.fillRect(food.x, food.y, box, box);
 }
@@ -34,21 +34,21 @@ function drawfood(){
 
 document.addEventListener("keydown", update);
 
-function update (event) {
-    if(event.keyCode == 37 && direction != "right") direction = "left";
-    if(event.keyCode == 38 && direction != "down") direction = "up";
-    if(event.keyCode == 39 && direction != "left") direction = "right";
-    if(event.keyCode == 40 && direction != "up") direction = "down";
+function update(event) {
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
 
 }
 
 
 function iniciarJogo() {
 
-    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-    if(snake[0].x < 0  && direction == "left") snake[0].x = 16 * box;
-    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-    if(snake[0].y < 0  && direction == "up") snake[0].y = 16 * box;
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
 
     drawBG();
@@ -63,7 +63,14 @@ function iniciarJogo() {
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
 
-    snake.pop();
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box,
+        food.y = Math.floor(Math.random() * 15 + 1) * box
+    }
+
+
 
     let newHead = {
         x: snakeX,
