78a424494078c4a0b0827993d945cc7c2b7630af
Luan <luanrschulzz@gmail.com>
2022-08-19 12:10:57 -0300

    Colisão implementaDA

--- js/main.js
+++ js/main.js
@@ -50,7 +50,13 @@ function iniciarJogo() {
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
-
+    for(i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Você perdeu :( ")
+        }
+    }
+ 
     drawBG();
     drawSnake();
     drawfood();
@@ -69,8 +75,6 @@ function iniciarJogo() {
         food.x = Math.floor(Math.random() * 15 + 1) * box,
         food.y = Math.floor(Math.random() * 15 + 1) * box
     }
-
-
 
     let newHead = {
         x: snakeX,
